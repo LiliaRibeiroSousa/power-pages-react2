37b5c76fe57e633c03b177c231116db5ffc656e4
Lilia Sousa <lilia.sousa@golegal.law>
2025-02-28 14:01:23 +0100

    Fix case-sensitive file import

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Contacts from "./Pages/UserManagement.jsx";
+import Contacts from "./pages/UserManagement.jsx";
 
 
 
